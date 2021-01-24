c9cd489f3100ed96e0919827eeedd03c86cf96d9
idoaviv19031@hotmail.com <idoaviv19031@hotmail.com>
2021-01-24 14:32:49 +0200

    login/logout

---
+++ TamagotchiClient/DataTransferObjects/PlayerDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TamagotchiClient.DataTransferObjects
+{
+    public class PlayerDTO
+    {
+        public int PlayerId { get; set; }
+        public string PlayerEmail { get; set; }
+        public string PlayerUserName { get; set; }
+        public string PlayerGender { get; set; }
+        public DateTime? PlayerBirthDate { get; set; }
+        public string PlayerFirstName { get; set; }
+        public string PlayerLastName { get; set; }
+        public int? PlayerActiveAnimal { get; set; }
+        public string PlayerPassword { get; set; }
+
+    }
+}
--- TamagotchiClient/WebClient/TamagotchiClient.cs
+++ TamagotchiClient/WebClient/TamagotchiClient.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
+using TamagotchiClient.DataTransferObjects;
 
 namespace TamagotchiClient.WebClient
 {
@@ -40,6 +41,47 @@ namespace TamagotchiClient.WebClient
             {
                 return "";
             }
+        }
+
+        public async Task<PlayerDTO> LoginAsync(UserDTO user)
+        {
+            string url = this.baseUrl + "/Login";
+            try
+            {
+                string json = JsonSerializer.Serialize(user);
+                StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, stringContent); 
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<PlayerDTO>(content);
+                }
+                else
+                    return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public async Task<bool> Logout()
+        {
+            string url = this.baseUrl + "/Logout";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
         }
     }
 }
